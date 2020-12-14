220a428da13e52c88f19eabe36d4f083c2dca634
virtuosyo <32742330+Yanggoing@users.noreply.github.com>
2020-12-14 22:53:17 +0800

    Chore: StoryBook Reboot

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { withInfo } from "@storybook/addon-info";
 import "../src/styles/index.scss";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { themes } from "@storybook/theming";
+
+library.add(fas)
 
 const wrapperStyle: React.CSSProperties = {
   padding: "20px 40px",
@@ -14,10 +17,9 @@ export const decorators = [
       {Story()}
     </div>
   ),
-  withInfo,
 ];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   info: { inline: true, header: false },
-}; 
+};
