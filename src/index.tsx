ce1eb2fe063f478a4d6012d2aaeeee15529dbd5b
virtuosyo <32742330+Yanggoing@users.noreply.github.com>
2020-12-15 23:43:20 +0800

    Build: index

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export { default as Button } from './component/Button'
+export { default as Menu } from './component/Menu'
+export { default as AutoComplete } from './component/AutoComplete'
+export { default as Icon } from './component/Icon'
+export { default as Input } from './component/Input'
+export { default as Progress } from './component/Progress'
+export { default as Transition } from './component/Transition'
+export { default as Upload } from './component/Upload'
